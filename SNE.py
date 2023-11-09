d231679822c29e09d2fc7104053c0623880b7dcb
tatuya <nakao.tatuya.2000@gmail.com>
2023-11-09 20:09:35 +0900

    deleted part od codes

--- SNE.py
+++ SNE.py
@@ -10,14 +10,3 @@ col = 5.0
 #取得データの確認
 print("inputed data:")
 print(X)
-
-#データ行列とその転置した行列を掛け算する
-#XX = np.dot(X, X.T)
-
-#Covの固有値と固有ベクトルを取得
-#eig_val, eig_vec = em.calc_sorter_eigen(XX)
-
-#print("eigen value:")
-#print(eig_val)
-#print("eigen vector:")
-#print(eig_vec)

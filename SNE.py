e488943497d8a894ef66a25b09d61193bb487b79
tatuya <nakao.tatuya.2000@gmail.com>
2023-11-12 18:57:19 +0900

    modified illustrator and SNE

--- SNE.py
+++ SNE.py
@@ -2,23 +2,23 @@ import numpy as np
 from functools  import reduce
 import illustrator as ill
 import binary_search as bs
-import matplotlib.pyplot as plt
 
 #データの取得
 X = np.loadtxt('data.csv')
 row = 5
-col = 5
+col = X.shape[0]
 
 #取得データの確認
 print("inputed data:")
 print(X)
 
+learning_rate = 0.005
+momentum = 0.01
+optimaize_loop = 1500
 Perplexity = 1.3
+threshold = 0.00001
 log_perp = np.log2(Perplexity)
 print("log_perp: " + str(log_perp))
-
-sigma = 500.0
-compressed_data = np.random.rand(row, 2)
 
 def d_pow2(vec1, vec2):
     return np.dot((vec1 - vec2), (vec1 - vec2))
@@ -60,58 +60,72 @@ def Q(X, i, j):
         return d_ij / sum
 
 def Entropy(sigma, X, i):
-    entropies = [P(X,i,j,sigma) for j in range(row) if i!=j]
-    #print("each entropies: ")
-    #print(entropies)
+    num = X.shape[0]
+    entropies = [P(X,i,j,sigma) for j in range(num) if i!=j]
     return sum([-p * np.log2(p) for p in entropies if p > 0.0])
 
 def KLdivergence(X, Y, sigmas):
-    for i in range(row):
-        p_ji = np.array([P(X,i,j,sigmas[i]) for j in range(row) if i!=j])
-        q_ji = np.array([Q(Y,i,j          ) for j in range(row) if i!=j])
+    num = X.shape[0]
+    for i in range(num):
+        p_ji = np.array([P(X,i,j,sigmas[i]) for j in range(num) if i!=j])
+        q_ji = np.array([Q(Y,i,j          ) for j in range(num) if i!=j])
 
         amount_info = p_ji * np.log2(p_ji / q_ji)
-        #print(amount_info)
 
         return sum(amount_info)
 
-sigma = bs.binary_search(Entropy, (X, 0), log_perp, 1.0e-7, [1.0001, 100000.0], 100)
 sigmas = [
     bs.binary_search(Entropy, (X, i), log_perp, 1.0e-7, [1.0001, 100000.0], 100)
     for i in range(row)
     ]
 print("sigmas: " + str(sigmas))
 
-entropy = [Entropy(sigmas[i],X,i) for i in range(row)]
-#print("whole entropy: ")
-#print(entropy)
-
-print("KLdivergence: ")
-print(KLdivergence(X, compressed_data, sigmas))
-
 def grad(X, Y, i, sigmas):
-    p_ji = np.array([P(X, i, j, sigmas[i]) for j in range(row) if i!=j])
-    q_ji = np.array([Q(Y, i, j           ) for j in range(row) if i!=j])
-    p_ij = np.array([P(X, j, i, sigmas[j]) for j in range(row) if i!=j])
-    q_ij = np.array([Q(Y, j, i           ) for j in range(row) if i!=j])
-    y_i  = np.array([Y[i] - Y[j]           for j in range(row) if i!=j])
+    num = X.shape[0]
+    p_ji = np.array([P(X, i, j, sigmas[i]) for j in range(num) if i!=j])
+    q_ji = np.array([Q(Y, i, j           ) for j in range(num) if i!=j])
+    p_ij = np.array([P(X, j, i, sigmas[j]) for j in range(num) if i!=j])
+    q_ij = np.array([Q(Y, j, i           ) for j in range(num) if i!=j])
+    y_i  = np.array([Y[i] - Y[j]           for j in range(num) if i!=j])
     
-    pq = (p_ji - q_ji + p_ij - q_ij).reshape(1, row-1)
+    pq = (p_ji - q_ji + p_ij - q_ij).reshape(1, num-1)
     return 2*np.dot(pq, y_i)[0]
 
-KL = []
-for i in range(1000):
-    gradient = np.array([grad(X, compressed_data, i, sigmas) for i in range(row)])
-    compressed_data -= 0.01 * gradient
-    #print("KLdivergence: ")
-    KL.append(KLdivergence(X, compressed_data, sigmas))
+def SNE(X, dim):
+    num = X.shape[0]
+    Y = np.random.rand(row, dim)
+    Y_1 = np.zeros([num, dim])
+    Y_2 = np.zeros([num, dim])
+    
+    frames = []
+    frames.append(ill.illustrate(Y, margin=0.3))
+    cost = KLdivergence(X, Y, sigmas)
+    KL = []
+    KL.append(cost)
+    for i in range(optimaize_loop):
+        gradient = np.array([grad(X, Y, i, sigmas) for i in range(num)])
+        Y -= learning_rate * gradient
+        Y += momentum * (Y_1 - Y_2)
+        frames.append(ill.illustrate(Y, margin=0.3))
 
+        new_cost = KLdivergence(X, Y, sigmas)
+        KL.append(new_cost)
+        if(abs(new_cost - cost) < threshold):
+            break
+
+        Y_2 = Y_1
+        Y_1 = gradient
+        cost = new_cost
+
+    ill.seve_animation(frames, "SNE")
+    return Y, KL
+
+compressed_data, KL = SNE(X, 2)
 
 print("KLdivergence: ")
 print(KLdivergence(X, compressed_data, sigmas))
-
-
-plt.plot(KL)
-plt.show()
+ill.line_chart(KL)
+ill.show()
 
 ill.illustrate(compressed_data, margin=0.3)
+ill.show()
